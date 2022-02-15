fbfde13e330977d95a95eeed3cd7bb57a0c7b9cb
Kudryavtcev A.A <installer.233@gmail.com>
2022-02-15 11:54:55 +0300

    я добавил приколы

--- Практика/Program.cs
+++ Практика/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string  a = Console.ReadLine();
+if (a == "g")
+{
+    Console.WriteLine("you are the best");
+}
